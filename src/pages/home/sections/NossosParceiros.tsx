527d9ad2ee9ca3498d5874a629557803c1e391c8
Samuel Luz Santana <samuelluzsantana@outlook.com>
2024-05-30 15:31:53 -0300

    Nosdsos

--- src/pages/home/sections/NossosParceiros.tsx
+++ src/pages/home/sections/NossosParceiros.tsx
@@ -5,9 +5,17 @@ export default function NossosParceiros() {
     return (
         <>
             <div className="flex relative bottom-[8em] z-10 h-[50em] bg-white">
-                <p>
-                    Nossos Parceiros
-                </p>
+
+                <div className="nossos-parceiros-header mt-[10em] mx-[15rem] flex flex-col">
+
+                    <h3 className=" text-3xl text-loguin-blue font-bold">
+                        Nossos Parceiros
+                    </h3>
+
+                    <p className="font-normal">Quem confia, cresce junto: a força dos nossos parceiros
+
+                    </p>
+                </div>
             </div>
         </>
     )
